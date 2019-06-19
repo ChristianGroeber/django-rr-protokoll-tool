d516561c1700ff07033163fe19c14a3e72e54ef6
christiangroeber <christian.groeber@bluewin.ch>
2019-06-19 11:40:42 +0200

    implemented login and logout

--- rr_protokoll_tool/tool/views.py
+++ rr_protokoll_tool/tool/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth import forms, authenticate, login, logout
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 
 
 def index(request):
+    if str(request.user) is 'AnonymousUser':
+        return redirect('login')
     return render(request, 'tool/index.html')
+
+
+def user_login(request):
+    if str(request.user) is not 'AnonymousUser':
+        return redirect('home')
+    form = forms.AuthenticationForm(request.POST or None)
+    if request.method == 'GET':
+        return render(request, 'tool/login.html', {'form': form})
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('home')
+
+
+def user_logout(request):
+    logout(request.user)
+    return redirect('login')
