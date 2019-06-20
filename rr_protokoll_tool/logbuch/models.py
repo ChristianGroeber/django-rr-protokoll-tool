2a628529bf33cf35668ad936dbffd6db7cdadb4a
christiangroeber <christian.groeber@bluewin.ch>
2019-06-20 22:07:51 +0200

    changed scss, added font

--- rr_protokoll_tool/logbuch/models.py
+++ rr_protokoll_tool/logbuch/models.py
@@ -3,10 +3,17 @@ from django.db.models import ImageField
 # Create your models here.
 
 
-class Abzeichen(models.Model):
+class Aufgabe(models.Model):
+    beschreibung = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.beschreibung
+
+
+class UnterThema(models.Model):
     name = models.CharField(max_length=100)
-    beschreibung = models.CharField(max_length=500, blank=True)
-    abbildung = ImageField(upload_to='abzeichen', blank=True)
+    anmerkung = models.CharField(max_length=200, blank=True)
+    aufgaben = models.ManyToManyField(Aufgabe, blank=True)
 
     def __str__(self):
         return self.name
@@ -14,29 +21,21 @@ class Abzeichen(models.Model):
 
 class Thema(models.Model):
     name = models.CharField(max_length=100)
-    gehoert_zu_abzeichen = models.ForeignKey(Abzeichen, on_delete=models.CASCADE)
+    unterthemen = models.ManyToManyField(UnterThema, blank=True)
 
     def __str__(self):
         return self.name
 
 
-class UnterThema(models.Model):
+class Abzeichen(models.Model):
     name = models.CharField(max_length=100)
-    anmerkung = models.CharField(max_length=200, blank=True)
-    gehoert_zu = models.ForeignKey(Thema, on_delete=models.CASCADE)
+    beschreibung = models.CharField(max_length=500, blank=True)
+    abbildung = ImageField(upload_to='abzeichen', blank=True)
+    themen = models.ManyToManyField(Thema, blank=True)
 
     def __str__(self):
         return self.name
-
-
-class Aufgabe(models.Model):
-    beschreibung = models.CharField(max_length=500)
-    gehoert_zu_thema = models.ForeignKey(Thema, on_delete=models.SET_NULL, null=True, blank=True)
-    gehoert_zu_unter_thema = models.ForeignKey(UnterThema, on_delete=models.SET_NULL, null=True, blank=True)
-
-    def __str__(self):
-        return self.beschreibung
 
 
 class Logbuch(models.Model):
-    aufgaben = models.ManyToManyField(Aufgabe, blank=True)
+    abzeichen = models.ManyToManyField(Abzeichen, blank=True)
